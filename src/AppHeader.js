6f29cdd540e3b7bd791c4c3e86275ab623e04e51
Max Gerlock <maxgerlock@gmail.com>
2020-09-20 13:02:13 -0700

    add app logo

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './AppHeader.css';
+import logo from './img/musical_messenger_logo_2.png';
 import { BsMusicNote } from 'react-icons/bs';
 
 class AppHeader extends Component {
@@ -15,6 +16,12 @@ class AppHeader extends Component {
 
   }
 
+  renderLogo() {
+    return (
+      <img className='mm-logo' src={logo} alt="Musical Messenger Logo" />
+    );
+  }
+
   render() {
     return (
       <div className='App-header'>
@@ -25,7 +32,7 @@ class AppHeader extends Component {
           onClick={this.props.toggleMusical}
           title='Toggle musical audiation of messages'/>
         </div>
-        <h1> Musical Messenger </h1>
+        {this.renderLogo()}
       </div>
     );
   }
